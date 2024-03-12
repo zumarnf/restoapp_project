9743231a572d5ee06726b58ece592399b0de6102
zumzum <kazonima@gmail.com>
2024-03-12 21:03:21 +0700

    update_report_2

--- report.php
+++ report.php
@@ -2,13 +2,32 @@
 require_once("services/database.php");
 require_once("services/pdf/fpdf.php");
 
+
 if (isset($_POST['report'])) {
     $hari = $_POST['hari'];
     $pdf = new FPDF();
     $pdf->AddPage();
     $pdf->SetTitle("Laporan Pengunjung");
     $pdf->SetFont("Arial", "B", 14);
-    $pdf->Cell(40, 10, "Test", 1, 1);
+
+    $pdf->Text(10, 6, "Laporan Pengunjung Tanggal $hari");
+
+    $select_history_query = "SELECT * FROM history WHERE hari='$hari'";
+    $select_history = $db->query($select_history_query);
+
+    $pdf->Cell(22, 10, "no_meja", 1, 0);
+    $pdf->Cell(44, 10, "nama_pelanggan", 1, 0);
+    $pdf->Cell(38, 10, "hari_keluar", 1, 0);
+    $pdf->Cell(38, 10, "jam_keluar", 1, 0);
+    $pdf->Cell(40, 10, "", 0, 1);
+
+    foreach ($select_history as $history) {
+        $pdf->Cell(22, 10, $history["no_meja"], 1, 0);
+        $pdf->Cell(44, 10, $history["nama_pelanggan"], 1, 0);
+        $pdf->Cell(38, 10, $history["hari"], 1, 0);
+        $pdf->Cell(38, 10, $history["jam"], 1, 1);
+    }
+
     $pdf->Output();
 }
 
