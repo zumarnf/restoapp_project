6cc9eea2999b7d1e69240500c8f5b625dee566f5
zumzum <kazonima@gmail.com>
2024-03-13 13:06:32 +0700

    report_update3

--- layouts/header.php
+++ layouts/header.php
@@ -1,14 +1,14 @@
 <header>
     <div>
-        <a href="index.php">
-            Bro Resto
-        </a>
+        <a href="index.php">BRO RESTO</a>
     </div>
     <div>
         <?php
         if (isset($_SESSION['is_login'])) {
+            echo "<div class='right'>";
+            echo "<a href='report.php'>report</a>";
             echo "<a href='logout.php'>logout</a>";
-
+            echo "</div>";
         } else {
             echo "<a href='login.php'>login</a>";
         }
--- report.php
+++ report.php
@@ -1,34 +1,42 @@
 <?php
-require_once("services/database.php");
 require_once("services/pdf/fpdf.php");
+require_once("services/database.php");
 
+session_start();
+
+if ($_SESSION['is_login'] == false) {
+    header('location: login.php');
+}
 
 if (isset($_POST['report'])) {
     $hari = $_POST['hari'];
     $pdf = new FPDF();
     $pdf->AddPage();
     $pdf->SetTitle("Laporan Pengunjung");
-    $pdf->SetFont("Arial", "B", 14);
-
-    $pdf->Text(10, 6, "Laporan Pengunjung Tanggal $hari");
+    $pdf->setFont("Arial", "B", 14);
 
     $select_history_query = "SELECT * FROM history WHERE hari='$hari'";
     $select_history = $db->query($select_history_query);
 
-    $pdf->Cell(22, 10, "no_meja", 1, 0);
-    $pdf->Cell(44, 10, "nama_pelanggan", 1, 0);
-    $pdf->Cell(38, 10, "hari_keluar", 1, 0);
-    $pdf->Cell(38, 10, "jam_keluar", 1, 0);
-    $pdf->Cell(40, 10, "", 0, 1);
-
-    foreach ($select_history as $history) {
-        $pdf->Cell(22, 10, $history["no_meja"], 1, 0);
-        $pdf->Cell(44, 10, $history["nama_pelanggan"], 1, 0);
-        $pdf->Cell(38, 10, $history["hari"], 1, 0);
-        $pdf->Cell(38, 10, $history["jam"], 1, 1);
+    if ($select_history->num_rows > 0) {
+        $pdf->Text(10, 6, "Total $select_history->num_rows pengunjung pada $hari");
+        $pdf->Cell(24, 10, "no_meja", 1, 0);
+        $pdf->Cell(48, 10, "nama_pelanggan", 1, 0);
+        $pdf->Cell(38, 10, "hari keluar", 1, 0);
+        $pdf->Cell(38, 10, "jam keluar", 1, 0);
+        $pdf->Cell(40, 10, "", 0, 1);
+        foreach ($select_history as $history) {
+            $pdf->Cell(24, 10, $history["no_meja"], 1, 0);
+            $pdf->Cell(48, 10, $history["nama_pelanggan"], 1, 0);
+            $pdf->Cell(38, 10, $history["hari"], 1, 0);
+            $pdf->Cell(38, 10, $history["jam"], 1, 1);
+        }
+        $pdf->Output();
+    } else {
+        $pdf->setFont("Arial", "B", 14);
+        $pdf->Cell(38, 10, "Tidak ada laporan untuk tanggal $hari", 0, 1);
+        $pdf->Output();
     }
-
-    $pdf->Output();
 }
 
 ?>
@@ -40,11 +48,12 @@ if (isset($_POST['report'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css" />
-    <title>Report</title>
+    <title>REPORT</title>
 </head>
 
 <body>
     <?php include("layouts/header.php") ?>
+
     <div class="super-center">
         <h3>Cetak PDF</h3>
         <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
